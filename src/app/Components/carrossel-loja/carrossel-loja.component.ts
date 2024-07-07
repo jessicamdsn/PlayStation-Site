846e85031f0d020da5bde389d51a1da67517d6d4
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-07-07 11:38:22 -0300

    feat: adiciona biblioteca swiper

---
+++ src/app/Components/carrossel-loja/carrossel-loja.component.ts
@@ -0,0 +1,66 @@
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-carrossel-loja',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carrossel-loja.component.html',
+  styleUrl: './carrossel-loja.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class CarrosselLojaComponent {
+  slides = [
+    {
+      headline: 'Console PlayStation 5',
+      content: 'Descubra a mais nova geração de jogos incríveis para PlayStation.',
+      img: '../../../assets/loja/ps5.png',
+    },
+    {
+      headline: 'Controle sem fio DualSense™',
+      content: 'Mergulhe na experiência de jogo com um controle que suporta resposta tátil e efeitos de gatilho dinâmicos.',
+      img: '../../../assets/loja/controle-dualsense.png',
+    },
+    {
+      headline: 'Headset sem fio PULSE Elite™',
+      content: 'Ouça áudio de jogos mais realista neste modelo de headset confortável e equipado com microfone retrátil e bateria embutida de longa duração',
+      img: '../../../assets/loja/headset-pulse.png',
+    },
+    {
+      headline: 'Fones de ouvido sem fio PULSE Explore™',
+      content: 'Curta sons realistas aonde quer que os jogos levem você com um design portátil equipado com microfones ocultos e um estojo de carregamento para acompanhar.',
+      img: '../../../assets/loja/earbuds-pulse.png',
+    },
+    {
+      headline: 'Controle sem fio DualSense Edge™',
+      content: 'Entre com tudo nos jogos com botões remapeáveis, gatilhos e controles ajustáveis, diferentes capas para controle analógico, botões traseiros e mais.',
+      img: '../../../assets/loja/controle-semfio.png',
+    },
+    {
+      headline: 'Controle Access™',
+      content: 'Um controle personalizável para PlayStation®5 criado para tornar os jogos mais acessíveis.',
+      img: '../../../assets/loja/controle-access.png',
+    },
+    {
+      headline: 'Tampas do console PS5',
+      content: 'Personalize seu console PlayStation 5 ou PlayStation 5 Edição Digital com uma variedade vibrante de novas opções de cores.',
+      img: '../../../assets/loja/ps5-tampa.png',
+    },
+    {
+      headline: 'Controle de mídia',
+      content: 'Conveniência e um layout intuitivo para você controlar filmes, serviços de streaming e muito mais no seu console PS5.',
+      img: '../../../assets/loja/controle-media.png',
+    },
+    {
+      headline: 'Câmera HD',
+      content: 'Adicione sua própria imagem aos vídeos de jogabilidade e transmissões com uma captura suave, nítida e Full HD.',
+      img: '../../../assets/loja/camera-hd.png',
+    },
+  ];
+
+  currentSlide = 0;
+
+  changeSlide(index: number) {
+    this.currentSlide = index;
+  }
+}
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../Components/nav/nav.component';
 import { CarrosselComponent } from '../Components/carrossel/carrossel.component';
+import { CarrosselLojaComponent } from '../Components/carrossel-loja/carrossel-loja.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavComponent, CarrosselComponent],
+  imports: [NavComponent, CarrosselComponent, CarrosselLojaComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
