0b924ec226f74e5cace3fdb2f64bdbb170f54d67
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-07-07 20:18:12 -0300

    feat: seção playstation plus

---
+++ src/app/Components/play-plus/play-plus.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-play-plus',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './play-plus.component.html',
+  styleUrl: './play-plus.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class PlayPlusComponent {
+
+}
